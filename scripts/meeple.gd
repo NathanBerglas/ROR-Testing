108515db47c113a061635861aac3e72dc34261b6
Jacob ElzingaCheng <jacob.elzingacheng@gmail.com>
2025-06-23 09:30:45 -0400

    Added select based movement to the meeple

--- scripts/meeple.gd
+++ scripts/meeple.gd
@@ -5,19 +5,49 @@ extends Node2D
 @export var area_2d: Area2D
 @export var speed: float = 200
 @export var acceleration: float = 300
+
+var selected = false #Determines if a meeple is selected
+var dest = null #destination of a meeple
+
+
 var min_distance = 9 # Squared
 
-var target = Vector2(0,0)
 
-func _physics_process(delta):
-	_go_to_target(delta)
+#var target = Vector2(0,0)
 
+func _physics_process(delta): #runs on each meeple every tick
+	
+	if (dest != null): #if a meeple has somewhere to go, goes to it
+		_go_to_target(delta)
+		
+	if dest != null and closeEnough(): #meeple reaches destination
+		dest = null
+	
+	
+func closeEnough(): #checking if a meeple is close enough to their destination
+	var dist = rb.global_position - dest
+	if ((dist.x * dist.x) < 3 and (dist.y * dist.y) < 3):
+		return true
+	return false
+	
+func hasMouse(): #Checks if the mouse is within the meeple
+	var radius = 800 #should be set to meeple radius squared
+	var mousePos = get_global_mouse_position()
+	
+	var dif = rb.global_position - mousePos
+	
+	if (dif.x * dif.x) < radius and (dif.y * dif.y)< radius:
+		return true
+	return false
+	
+	
 # Marches Meeple to Target
 func _go_to_target(delta):
-	var to_target = target - rb.global_position
+	var to_target = dest - rb.global_position
 	var dist = to_target.length_squared()
 	var speed_towards_target = rb.linear_velocity.dot(to_target.normalized())
 	
 	if dist > min_distance and speed_towards_target < speed: # Checks if meeple is close to target point
 		var force = to_target.normalized() * min(acceleration, abs(speed_towards_target-speed) / delta) # If acceleration would overshoot
 		rb.apply_central_force(force)
+		
--- scripts/meeple_control.gd
+++ scripts/meeple_control.gd
@@ -6,20 +6,45 @@ extends Node2D
 var group: Array[Array] = [[]]
 var group_targets: Array[Vector2] = [Vector2(1000,500)]
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("spawn_meeple"):
 		var instance = meeple_prefab.instantiate()
 		
 		# Set instance's data
+		
 		instance.global_position = get_global_mouse_position()
-		instance.target = group_targets[0]
+		#instance.target = group_targets[0]
 		
 		# Create instance
 		add_child(instance)
 		group[0].push_back(instance)
 		print("Spawned meeple")
+		
+		
+	elif Input.is_action_just_pressed("super_order"):
+		group_targets[0] = get_global_mouse_position()
+		targetMarker.global_position = group_targets[0]
+		
+		for m in group[0]:
+			m.dest = group_targets[0]
+			
+			
 	elif Input.is_action_just_pressed("order"):
 		group_targets[0] = get_global_mouse_position()
 		targetMarker.global_position = group_targets[0]
 		for m in group[0]:
-			m.target = group_targets[0]
+			if m.selected:
+				m.dest = group_targets[0]
+				m.selected = false
+			
+	elif Input.is_action_just_pressed("select"):
+		var hit = false
+		for m in group[0]:
+			if m.hasMouse():
+				m.selected = true
+				hit = true
+		if hit == false:
+			for m in group[0]:
+				m.selected = false
+			
+	
