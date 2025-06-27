3b7e6834af5c4033178d6c91cc20dfc1487d6aed
Jacob ElzingaCheng <jacob.elzingacheng@gmail.com>
2025-06-27 01:05:34 -0400

    Added Logic to detect if a building is placeable, and fixed actually placing a building

--- scripts/Buildings/buildingControl.gd
+++ scripts/Buildings/buildingControl.gd
@@ -3,8 +3,9 @@ extends Node2D
 @onready var farmButton = $buildingHud/FarmButton
 @export var farm_prefab: PackedScene
 
+#
 #Players Money
-var money = 1000
+var money = 10000
 
 #Checking what type of building the player is dragging
 var buildingDraggin = null
@@ -16,49 +17,59 @@ var buildings = []
 func _ready(): #Runs on start, connects buttons
 	farmButton.button_down.connect(_on_farm_button_pressed)
 	farmButton.button_up.connect(_on_farm_button_released)
+	farmButton.custom_minimum_size = Vector2(121.6,120)
 	
 
 #Start dragging the farm if has enough money
 func _on_farm_button_pressed():
 	if money < 500:
+		print("Ya Broke")
 		return
 	buildingDraggin = "Farm"
-	
-
-#finishes dragging the farm and places
-func _on_farm_button_released():
-	if buildingDraggin != "Farm":
-		return
-	money -= 500
-	hud.updateMoney(money) #updates money amount
-	farmButton.position = hud.farmButtonPos
-	buildingDraggin = null
-
-	var instance = farm_prefab.instantiate() #New money farm
+	#money -= 500
+	#Adding the farm to be dragged to the list of buildings
+	# hashtag-No way this will cause errors in the future
+	var instance = farm_prefab.instantiate() #New FAKE money farm
+	instance.fake = true
 	var toAdd = ["Farm", get_global_mouse_position(), instance]
 	
 	instance.global_position = get_global_mouse_position()
 	add_child(instance) #Adding the instance
-	buildings.push_back(toAdd) #Adding building to list of buildings owned
+	
+	buildings.push_back(toAdd) 
 	
 
+#finishes dragging the farm and places
+func _on_farm_button_released():
+	
+	if buildingDraggin != "Farm" or money < 500:
+		buildingDraggin = null
+		return
+	buildingDraggin = null
+	
+	if !buildings[buildings.size() - 1][2].is_placeable():
+		
+		buildings.pop_back()[2].queue_free()
+		return
+	buildings[buildings.size() - 1][2].fake = false
+	money -= 500
+	
 func _process(delta): #runs every tick
 	if buildingDraggin == "Farm": #Code actually dragging the farm around
-		var vector = get_global_mouse_position()
-		vector.x -= 60
-		vector.y -= 60
-		farmButton.position = vector
-	
+		buildings[buildings.size() - 1][2].global_position = get_global_mouse_position()
+		if !buildings[buildings.size() - 1][2].is_placeable():
+			buildings[buildings.size() - 1][2].shapey.modulate = Color(250, 0, 4) #RED
+		else:
+			buildings[buildings.size() - 1][2].shapey.modulate = Color(1, 1, 1)  # Reset to white
 
 	for m in buildings: #Doing tick stuff for each building
 		if m[0] == "Farm":
 			m[2].generateIncome(self, delta)
-
+	hud.updateMoney(money) #updates money amount
 	
 
 func addMoney(moneyChange): #Changing money
 	money += moneyChange
 	hud.updateMoney(money)
-	
 	
 	
--- scripts/Buildings/money_farm.gd
+++ scripts/Buildings/money_farm.gd
@@ -1,16 +1,38 @@
 extends Building
+@onready var shapey = $RigidBody2D/Sprite2D
 
-var moneyTimer = 0
 
+var moneyTimer = 0 #Tracking when to give $$$
+
+var fake = false #Check if this is dragging or NAH
 
 func _ready(): #I EXIST!
-	print("Farm ready to grow food!")
+	if !fake: print("Farm ready to grow food!")
 
 
 
 func generateIncome(p, delta): #Generates income every 10 seconds
+	if fake: return
+	
 	moneyTimer += delta
 	if moneyTimer >= 10:
 		p.addMoney(200)
 		moneyTimer = 0
 	
+
+#Dont ask Jacob WTF this code is. ChatGPT wrote is
+func is_placeable() -> bool: #Only for if a building is FAKE
+	if !fake: return false #<- cept this line
+	print("Whats up")
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsShapeQueryParameters2D.new()
+	
+	query.shape = $RigidBody2D/BodyForFarm.shape
+	query.transform = $RigidBody2D/BodyForFarm.global_transform
+	query.collide_with_areas = true
+	query.collide_with_bodies = true
+	query.exclude = [$RigidBody2D.get_rid()]
+	var result = space_state.intersect_shape(query)
+	for m in result:
+		print("Hit: ", m.collider.name)
+	return result.is_empty()  # True = no collision, so placeable
