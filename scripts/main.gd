a1a13dbccc6d5e3271b1d590eb2eaf9f3670a334
Jacob ElzingaCheng <jacob.elzingacheng@gmail.com>
2025-06-30 20:56:48 -0400

    Wierd Scene things

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,7 @@ extends Node
 func _ready():
 	$MeepleControl.teammates.push_back($BuildingControl)
 	$BuildingControl.teammates.push_back($MeepleControl)
+	
 
 func _process(_delta): #runs every
 	var x = 0
