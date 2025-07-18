6bafd6af23d193aee32d3cdb9e974e657f9867e3
Jacob ElzingaCheng <jacob.elzingacheng@gmail.com>
2025-07-18 02:27:12 -0400

    Got script workin again

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,13 +7,15 @@ func _ready():
 	var index = 0
 	for i in GameManager.Players:
 		var currentPlayer = playerScene.instantiate()
-
+		
+		add_child(currentPlayer)
+		
 		currentPlayer.name = str(GameManager.Players[i].id)
 		currentPlayer.playerID = GameManager.Players[i].id
-		GameManager.Players[i].controllers.push_back($Player/BuildingControl)
-		GameManager.Players[i].controllers.push_back($Player/MeepleControl)
-		print("Added controllers")
-		add_child(currentPlayer)
+		
+		GameManager.Players[i].controllers.push_back(currentPlayer.c[0])
+		GameManager.Players[i].controllers.push_back(currentPlayer.c[1])
+		
 		for spawn in get_tree().get_nodes_in_group("PlayerSpawnPoints"):
 			if spawn.name == str(index):
 				currentPlayer.global_position = spawn.global_position
@@ -23,5 +25,8 @@ func _ready():
 	pass
 
 func _process(_delta): #runs every
-	var x = 0
-	#We vibin
+	#We as the kids say: are no longer vibin
+	for p in GameManager.Players:
+		if p == 1:
+			print("Hi")
+	
