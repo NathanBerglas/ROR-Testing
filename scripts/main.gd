45804f2335b5c95931818824cac798f0cd7e5922
Jacob ElzingaCheng <jacob.elzingacheng@gmail.com>
2025-07-31 00:48:02 -0400

    DOUBLE COMMIT

--- scripts/main.gd
+++ scripts/main.gd
@@ -36,7 +36,7 @@ func _process(delta): #runs every
 			for p in GameManager.Players:
 				if p == multiplayer.get_unique_id():
 					if GameManager.ownControllersSet == true and ableToSend == true:
-						print("Order " + str(orderNum) + "? DONT WORRY BOSS, I BE SENDIN INFO OF " + str(GameManager.Players[p].meepleInfo.size()) + " MEEPLES FROM " + str(p))
+						#print("Order " + str(orderNum) + "? DONT WORRY BOSS, I BE SENDIN INFO OF " + str(GameManager.Players[p].meepleInfo.size()) + " MEEPLES FROM " + str(p))
 						UpdatePlayerInfo.rpc_id(1, playerName, multiplayer.get_unique_id(), GameManager.Players[p].meepleInfo, orderNum)
 						ableToSend = false
 						orderNum += 1
@@ -54,14 +54,14 @@ func _process(delta): #runs every
 #If it is the server, it sends the server info to all the clients
 @rpc("any_peer")
 func UpdatePlayerInfo(name, id, c, o):
-	print("Shipment of " + str(c.size()) + " has been complete from id: " + str(id) + " to: " + str(multiplayer.get_unique_id()))
-	print("Under order number: " + str(o))
+	#print("Shipment of " + str(c.size()) + " has been complete from id: " + str(id) + " to: " + str(multiplayer.get_unique_id()))
+	#print("Under order number: " + str(o))
 	readySend.rpc_id(id)
 	for p in GameManager.Players:
 		if p == id:
 			GameManager.Players[p].meepleInfo = c
-			if c.size() > 0:
-				print("Beep boop bap: vew vew pew, meeple transfer COMPLETE")
+			#if c.size() > 0:
+				#print("Beep boop bap: vew vew pew, meeple transfer COMPLETE")
 	
 	if multiplayer.is_server():
 		for i in GameManager.Players:
