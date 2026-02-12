08a817d7c307a3097688a178c76f90ddd68a1389
Jacob ElzingaCheng <jacob.elzingacheng@gmail.com>
2026-02-12 01:11:31 -0500

    random 1 am changes/updates

---
+++ scenes/prefabs/MeeplePrefab/caravan.gd
@@ -0,0 +1,31 @@
+extends meeple
+
+
+
+var woodCarrying = 0
+var foodCarrying = 0
+var stoneCarrying = 0
+
+
+func _ready():
+	self.rb = $RigidBody2D
+	self.sprite = $RigidBody2D/Sprite2D
+	self.label = $RigidBody2D/Label
+	self.type = "Caravan"
+
+
+func _process(delta): #runs on each meeple every tick
+	label.text = str(HP)
+	
+	
+	
+	if (path != null and shouldBeMoving): #if a meeple has somewhere to go, goes to it
+		_go_to_target(delta)
+	
+	#if dest != null and closeEnough(): #meeple reaches destination
+		#dest = null
+	
+func get_id():
+	return self.UNIQUEID
+func set_id(id):
+	self.UNIQUEID = id
