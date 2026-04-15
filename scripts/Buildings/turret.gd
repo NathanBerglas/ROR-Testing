d64fb1c04a355673f3a0b7ff5e9ed476fdb4bf90
NathanBerglas <nathan.berglas@gmail.com>
2026-04-14 22:53:14 -0400

    Finished turret targeting and attacking

--- scripts/Buildings/turret.gd
+++ scripts/Buildings/turret.gd
@@ -4,28 +4,33 @@ extends Building
 @onready var HPBar = $HP_BAR
 
 var target = null
-const DMG = 3
-const RANGE = 5
-func attack():
-	if target == null:
-		return
-	target.hp -= DMG
-	
+const DMG = 1
+const SECONDS_PER_ATTACK = 2
+var time_since_last_attack = 0
+
+func attack(delta):
+	time_since_last_attack += delta
+	if time_since_last_attack > SECONDS_PER_ATTACK:
+		time_since_last_attack = 0
+		if target == null:
+			return
+		target.update_hp(-DMG)
+		if !is_instance_valid(target): # was just killed
+			target = null
+		
 func getTarget(grid):
-	print("-------------")
-	var tempDirs = []
-	
+	var visited_hexs = []
 	for d in grid.HEX_DIRS:
 		for h in grid.HEX_DIRS:
-			tempDirs.append(d + h)
-	
-	for t in tempDirs:
-		print(t)
-	print("--------------")
+			var tile_pos = self.pos + d + h
+			if !(tile_pos in visited_hexs):
+				visited_hexs.append(tile_pos)
+				var tile = grid.axial_probe(tile_pos)
+				if len(tile.objectsInside) > 0 and tile.objectsInside[0] is meeple:
+						target = tile.objectsInside[0]
+			
 #func _ready():
 	#$MultiplayerSynchronizer.set_multiplayer_authority()
-
-
 	
 func updateHPBar():
 	if self.fake:
