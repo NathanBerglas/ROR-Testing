7212a35b1f1cbf0aef96faa8b0675e150e82cad5
JElzingacheng <jacob.elzingacheng@gmail.com>
2026-04-24 18:16:04 -0400

    Added UPNP

--- scripts/main/main.gd
+++ scripts/main/main.gd
@@ -1,7 +1,7 @@
 extends Node
 
-@export var Address = "127.0.0.1"
-@export var port = 8910
+@export var Address = null
+@export var port = 9999
 
 
 @export var playerScene: PackedScene
@@ -28,6 +28,10 @@ var MAX_PLAYERS_AND_SERVER = 2
 var peer
 
 func _ready():
+	multiplayer.peer_connected.connect(peer_connected)
+	multiplayer.peer_disconnected.connect(peer_disconnected)
+	multiplayer.connected_to_server.connect(connected_to_server)
+	multiplayer.connection_failed.connect(connection_failed)
 	hostButton.button_down.connect(_on_host_button_pressed)
 	joinButton.button_down.connect(_on_join_button_pressed)
 	startButton.button_down.connect(_on_start_button_pressed)
@@ -88,6 +92,7 @@ func UpdatePlayerInfo(name, id, c, o):
 
 @rpc("any_peer")
 func readySend():
+	
 	ableToSend = true
 
 
@@ -104,11 +109,13 @@ func _on_host_button_pressed() -> void:
 	multiplayer.set_multiplayer_peer(peer)
 	waitingLabel.text = "WAITING FOR PLAYERS"
 	waitingLabel.visible = true
+	upnp_setup()
 
 
 func _on_join_button_pressed() -> void:
 
 	peer = ENetMultiplayerPeer.new()
+	Address = $LineEdit.text
 	var error = peer.create_client(Address, port)
 	
 	if error != OK:
@@ -126,9 +133,33 @@ func _on_start_button_pressed() -> void:
 	return
 	pass # Replace with function body.
 
+#Called on the server and all clients when someone connects
+func peer_connected(id):
+	if multiplayer.is_server():
+		print("Server Running")
+	else:
+		print("Player Connected: " + str(id))
+
+
+#Called on the server and all clients when someone connects
+func peer_disconnected(id):
+	print("Player Disconnected: " + str(id))
+
+
+#Only called on clients (Send info from clients to server)
+func connected_to_server():
+	print("Connected to server!")
+	#sendPlayerInfo.rpc_id(1, $LineEdit.text, multiplayer.get_unique_id(),null, null)
+
+
+#Only called on clients
+func connection_failed():
+	print("Connection failed :(")
+
 
 @rpc("authority")
 func start_game(biomeGenInfo): # [BMAP_RESOLUTIONx, BPIXELS_PER_TILE, BMAP_RESOLUTIONy, Bdebugging_grid = null, Bmap]
+	
 	hostButton.queue_free()
 	joinButton.queue_free()
 	startButton.queue_free()
@@ -167,3 +198,25 @@ func start_game(biomeGenInfo): # [BMAP_RESOLUTIONx, BPIXELS_PER_TILE, BMAP_RESOL
 		player.get_node("Grid").Bmap = biomeGenInfo[4]
 	player.playerID = multiplayer.get_unique_id()
 	add_child(player)
+	
+	
+	
+	
+	
+	
+	
+func upnp_setup():
+	var upnp = UPNP.new()
+	
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Discover Failed! Error %s" % discover_result)
+
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		"UPNP Invalid Gateway!")
+
+	var map_result = upnp.add_port_mapping(port)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address: %s" % upnp.query_external_address())
